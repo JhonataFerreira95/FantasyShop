4847bf730dee8d3fac313d847180d99d9685c3d6
JhonataOvirowa <jhonataovirowa@gmail.com>
2023-07-17 16:39:13 -0300

    Criado sale card e adição de novas imagens

--- src/components/cards/saleCard/saleCard.jsx
+++ src/components/cards/saleCard/saleCard.jsx
@@ -1,21 +1,22 @@
 import Image from 'next/image'
 
 import styles from './saleCard.module.css'
+import Button from '@/components/forms/button/button'
 
 export default function SaleCard() {
     return (
-        <div>
+        <div className={styles.salecard}>
             <Image src='/products/pixark.jpg' width={250} height={300} />
-            <div>
-                <h3>Oferta Exclusiva!</h3>
-                <div>
-                    <div>-75%</div>
-                    <div>
-                        <p>R$75,49</p>
-                        <h4>R$18,87</h4>
+            <div className={styles.info}>
+                <h3 className={styles.title}>Oferta Exclusiva!</h3>
+                <div className={styles.pricecard}>
+                    <div className={styles.percent}>-75%</div>
+                    <div className={styles.price}>
+                        <p className={styles.fullprice}>R$75,49</p>
+                        <h4 className={styles.discountprice}>R$18,87</h4>
                     </div>
                 </div>
-                <button>Adicionar ao carrinho</button>
+                <Button>Adicionar ao carrinho</Button>
             </div>
         </div>
 
